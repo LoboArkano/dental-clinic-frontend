7a04e970a59db5d35e98f457f2fa75f67b2182cd
LoboArkano <pipiolo_mananes@hotmail.com>
2021-01-26 11:15:18 -0600

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => (
+  <div>
+    <h1 className="brand">Dental Clinic</h1>
+    <ul>
+      <li>Treatments</li>
+      <li>Appointment</li>
+    </ul>
+    <div className="social" />
+  </div>
+);
+
+export default Header;
