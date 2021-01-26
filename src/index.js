ce77d0de7cb9f822dd53261a5b9bc85ff5559793
LoboArkano <pipiolo_mananes@hotmail.com>
2021-01-26 15:35:12 -0600

    Create store with thunkMiddleware

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import App from './components/App';
+import rootReducer from './reducers';
+
+const INITIAL_STATE = {
+  appointments: {
+    loading: false,
+    error: '',
+    list: [],
+    item: {},
+  },
+  doctors: {
+    loading: false,
+    error: '',
+    list: [],
+  },
+  treatments: {
+    loading: false,
+    error: '',
+    list: [],
+    item: {},
+  },
+};
+
+const store = createStore(rootReducer, INITIAL_STATE, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
