b2ba3bf8ec2f3d10774469919934d2885221dae8
LoboArkano <pipiolo_mananes@hotmail.com>
2021-01-27 23:36:32 -0600

    Create post user request function

---
+++ src/api/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const urlApi = 'http://localhost:3001/';
+
+export const postUserApi = async (state, opt = '') => {
+  const response = await axios.post(`${urlApi}${opt}`, {
+    user: {
+      name: state.name,
+      email: state.email,
+      password: state.password,
+      password_confirmation: state.password_confirmation,
+    },
+  },
+  { withCredentials: true });
+
+  return response.data;
+};
