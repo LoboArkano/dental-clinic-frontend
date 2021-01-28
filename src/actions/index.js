86f9e0c448f25614c1b37ce88148463df8058ca6
LoboArkano <pipiolo_mananes@hotmail.com>
2021-01-28 13:46:20 -0600

    Add actions to check session and logout

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,9 +3,12 @@ import {
   FETCH_APPOINTMENTS_SUCCESS, FETCH_APPOINTMENT_SUCCESS,
   FETCH_TREATMENTS_SUCCESS, FETCH_TREATMENT_SUCCESS,
   FETCH_DOCTORS_SUCCESS, FETCH_DOCTOR_SUCCESS,
-  POST_USER_SUCCESS,
+  POST_USER_SUCCESS, CHECK_SESSION_SUCCESS, LOGOUT_SUCCESS,
 } from './types';
-import { postUserApi, createSessionApi } from '../api/api';
+import {
+  postUserApi, createSessionApi,
+  checkSessionApi, logoutApi,
+} from '../api/api';
 
 export const fetchRequest = () => ({
   type: FETCH_REQUEST,
@@ -51,6 +54,16 @@ export const postUserSuccess = user => ({
   payload: user,
 });
 
+export const checkSessionSuccess = user => ({
+  type: CHECK_SESSION_SUCCESS,
+  payload: user,
+});
+
+export const logoutSuccess = status => ({
+  type: LOGOUT_SUCCESS,
+  payload: status,
+});
+
 export const postUser = (state, opt) => (
   dispatch => {
     dispatch(fetchRequest());
@@ -70,6 +83,30 @@ export const createSession = (state, opt) => (
       .then(response => {
         const user = response;
         return dispatch(postUserSuccess(user));
+      })
+      .catch(error => dispatch(fetchFailure(error.message)));
+  }
+);
+
+export const checkSession = opt => (
+  dispatch => {
+    dispatch(fetchRequest());
+    return checkSessionApi(opt)
+      .then(response => {
+        const user = response;
+        return dispatch(checkSessionSuccess(user));
+      })
+      .catch(error => dispatch(fetchFailure(error.message)));
+  }
+);
+
+export const logout = opt => (
+  dispatch => {
+    dispatch(fetchRequest());
+    return logoutApi(opt)
+      .then(response => {
+        const status = response;
+        return dispatch(logoutSuccess(status));
       })
       .catch(error => dispatch(fetchFailure(error.message)));
   }
