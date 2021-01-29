f8524ff0b28ba2e396b0360c39b077f6e3aea395
LoboArkano <pipiolo_mananes@hotmail.com>
2021-01-28 23:37:41 -0600

    Display treatments

--- src/containers/TreatmentList.js
+++ src/containers/TreatmentList.js
@@ -1,13 +1,20 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-import { logout } from '../actions/index';
+import { fetchTreatments, logout } from '../actions/index';
 import Error from '../components/Error';
 
 const TreatmentList = props => {
-  const { error, loggedInStatus } = props;
+  const {
+    error, loggedInStatus,
+    loading, list,
+  } = props;
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTreatments('treatments'));
+  }, []);
 
   const handleLogout = useCallback(() => {
     dispatch(logout('logout'));
@@ -23,10 +30,23 @@ const TreatmentList = props => {
 
   return (
     <>
-      <button type="button" onClick={handleLogout}>Logout</button>
-      <div>
-        Hello from TreatmentList
-      </div>
+      {
+        loading
+          ? <h3>Loading</h3>
+          : (
+            <>
+              <button type="button" onClick={handleLogout}>Logout</button>
+              {
+                list.treatments.map(treatment => (
+                  <div key={treatment.name}>
+                    <h3>{treatment.name}</h3>
+                    <p>{treatment.price}</p>
+                  </div>
+                ))
+              }
+            </>
+          )
+      }
     </>
   );
 };
@@ -34,13 +54,20 @@ const TreatmentList = props => {
 TreatmentList.propTypes = {
   error: PropTypes.string.isRequired,
   loggedInStatus: PropTypes.bool.isRequired,
+  loading: PropTypes.bool.isRequired,
+  list: PropTypes.arrayOf({
+    treatments: PropTypes.shape().isRequired,
+  }).isRequired,
 };
 
 const mapStateToProps = state => {
-  const { user } = state;
+  const { user, treatments } = state;
   const { error, loggedInStatus } = user;
+  const { loading, list } = treatments;
 
-  return { error, loggedInStatus };
+  return {
+    error, loggedInStatus, loading, list,
+  };
 };
 
 const mapDispatchToProps = {};
