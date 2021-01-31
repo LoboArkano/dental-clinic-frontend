345e655ffda846d990563a89dfe276ae4d0a9cf9
LoboArkano <pipiolo_mananes@hotmail.com>
2021-01-31 13:09:19 -0600

    Implement AppointmentList container

--- src/containers/AppointmentList.js
+++ src/containers/AppointmentList.js
@@ -1,13 +1,20 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import { connect, useDispatch } from 'react-redux';
-import { logout } from '../actions/index';
+import { checkSession, logout, fetchAppointments } from '../actions/index';
 import Error from '../components/Error';
 
 const AppointmentList = props => {
-  const { error, loggedInStatus } = props;
+  const {
+    error, loggedInStatus, list, loading,
+  } = props;
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(checkSession('logged_in'));
+    dispatch(fetchAppointments('appointments'));
+  }, []);
 
   const handleLogout = useCallback(() => {
     dispatch(logout('logout'));
@@ -23,10 +30,31 @@ const AppointmentList = props => {
 
   return (
     <>
-      <button type="button" onClick={handleLogout}>Logout</button>
-      <div>
-        Hello from AppointmentList
-      </div>
+      {
+      loading
+        ? <h3>Loading</h3>
+        : (
+          <>
+            <button type="button" onClick={handleLogout}>Logout</button>
+            <div>
+              Appointments
+              {
+                list.map(appointment => (
+                  <div key={appointment.id}>
+                    <h2>{`Appointment No: ${appointment.id}`}</h2>
+                    <p>{`Date: ${new Date(appointment.date).toGMTString()}`}</p>
+                    <p>{`Completed: ${appointment.completed}`}</p>
+                    <p>{`Doctor: ${appointment.doctor.name}`}</p>
+                    <p>{`Treatment: ${appointment.treatment.name}`}</p>
+                    <p>{`Price: ${appointment.treatment.price} USD`}</p>
+                    <p>{`Description: ${appointment.treatment.desc}`}</p>
+                  </div>
+                ))
+              }
+            </div>
+          </>
+        )
+      }
     </>
   );
 };
@@ -34,13 +62,18 @@ const AppointmentList = props => {
 AppointmentList.propTypes = {
   error: PropTypes.string.isRequired,
   loggedInStatus: PropTypes.bool.isRequired,
+  list: PropTypes.shape().isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = state => {
-  const { user } = state;
+  const { user, appointments } = state;
   const { error, loggedInStatus } = user;
+  const { list, loading } = appointments;
 
-  return { error, loggedInStatus };
+  return {
+    error, loggedInStatus, list, loading,
+  };
 };
 
 const mapDispatchToProps = {};
