4057fcdf75014f2e218eaeecd6b16feb29c94967
LoboArkano <pipiolo_mananes@hotmail.com>
2021-01-31 23:57:35 -0600

    Add style to Error component

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import '../assets/stylesheets/error.css';
 
 const Error = () => (
-  <div className="error-container">
+  <div className="error-container d-flex w-100">
     <h1>Ooops!! Page not found</h1>
   </div>
 );
