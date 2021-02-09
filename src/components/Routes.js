ece4fff522782402f56e77f09cd87d9a6114bfa7
LoboArkano <pipiolo_mananes@hotmail.com>
2021-02-09 02:12:03 -0600

    Move dispatch checkSession

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { connect, useDispatch } from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { checkSession } from '../actions/index';
 import Header from './Header';
 import Main from './Main';
 import Login from '../containers/Login';
@@ -10,22 +12,29 @@ import CAppointmentList from '../containers/AppointmentList';
 import AppointmentForm from '../containers/AppointmentForm';
 import Error from './Error';
 
-const Routes = () => (
-  <BrowserRouter>
-    <div className="d-flex w-100 vh-100">
-      <Header />
-      <Switch>
-        <Route path="/" component={Main} exact />
-        <Route path="/login" component={Login} exact />
-        <Route path="/sign-up" component={Registration} exact />
-        <Route path="/treatments" component={CTreatmentList} exact />
-        <Route path="/treatment/:id" component={CTreatment} exact />
-        <Route path="/appointments" component={CAppointmentList} exact />
-        <Route path="/appointment-form" component={AppointmentForm} exact />
-        <Route component={Error} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
+const Routes = () => {
+  const dispatch = useDispatch();
+  dispatch(checkSession('logged_in'));
 
-export default Routes;
+  return (
+    <BrowserRouter>
+      <div className="d-flex w-100 vh-100">
+        <Header />
+        <Switch>
+          <Route path="/" component={Main} exact />
+          <Route path="/login" component={Login} exact />
+          <Route path="/sign-up" component={Registration} exact />
+          <Route path="/treatments" component={CTreatmentList} exact />
+          <Route path="/treatment/:id" component={CTreatment} exact />
+          <Route path="/appointments" component={CAppointmentList} exact />
+          <Route path="/appointment-form" component={AppointmentForm} exact />
+          <Route component={Error} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+const mapDispatchToProps = {};
+
+export default connect(null, mapDispatchToProps)(Routes);
