b3db3af2b58d29aab543de9bfc7a3533d0d72d88
LoboArkano <pipiolo_mananes@hotmail.com>
2021-02-10 13:26:31 -0600

    Update urlApi

--- src/api/urlApi.js
+++ src/api/urlApi.js
@@ -1,4 +1,4 @@
-const urlApi = 'http://localhost:3001/';
-/* const urlApi = 'https://dental-clinic-api.herokuapp.com/'; */
+/* const urlApi = 'http://localhost:3001/'; */
+const urlApi = 'https://dental-clinic-api.herokuapp.com/';
 
 export default urlApi;
